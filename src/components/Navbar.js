b3a095491f97316cf6dee222ef4c8b2c4ca39e8a
mdshahzan <mdshahzan30@gmail.com>
2025-06-19 01:23:24 +0530

    Updated files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -83,7 +83,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/mdshahzan"
                 target="_blank"
                 className="fork-btn-inner"
               >
